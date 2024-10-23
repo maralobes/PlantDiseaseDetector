8dc74d01bfae83dd62e31a071affae7ec0184ddc
Maria <maria.villaloboschaves@gmail.com>
2024-10-23 13:05:09 -0600

    Upload and connect label_encoder file

--- app.py
+++ app.py
@@ -4,25 +4,20 @@ from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import LabelEncoder
+import joblib
 
 app = FastAPI()
 
+# Cargar el LabelEncoder guardado en la carpeta 'models'
+label_encoder = joblib.load('models/label_encoder.pkl')
+
+# Mostrar las clases almacenadas en el LabelEncoder
+print("Clases del LabelEncoder:")
+print(label_encoder.classes_)
+
 # Ruta del modelo
-MODEL_PATH = 'model/modelo_DEP.keras'
+MODEL_PATH = 'models/modelo_DEP.keras'
 model = tf.keras.models.load_model(MODEL_PATH)
-
-# Lista de clases con las que entrenaste el modelo
-etiquetas = ["Apple Black rot", "Apple Cedar apple rust", "Apple Healthy", "Apple Apple scab",
-             "Pepper bell Bacterial spot", "Pepper bell Healthy",
-             "Cherry (including sour) Powdery mildew", "Cherry (including sour) Healthy"]
-
-# Crear el LabelEncoder y ajustarlo con las clases
-label_encoder = LabelEncoder()
-label_encoder.fit(etiquetas)
-
-# Diccionario de clases generado por LabelEncoder
-class_names = {i: label for i, label in enumerate(label_encoder.classes_)}
 
 def preprocess_image(image: Image.Image):
     image = image.resize((128, 128))  # Ajusta al tamaño que espera el modelo
@@ -35,7 +30,7 @@ async def analyze_image(file: UploadFile = File(...)):
     try:
         # Leer la imagen subida
         image = Image.open(io.BytesIO(await file.read()))
-        image = image.convert('RGB')  # Asegúrate de que la imagen está en formato RGB
+        image = image.convert('RGB')  # Asegura que la imagen está en formato RGB
         
         # Preprocesar la imagen
         preprocessed_image = preprocess_image(image)
@@ -48,8 +43,8 @@ async def analyze_image(file: UploadFile = File(...)):
         predicted_class = np.argmax(predictions, axis=1)[0]
         print(f"Clase predicha (número): {predicted_class}")
 
-        # Mapeo de la clase a un nombre de enfermedad
-        class_label = class_names.get(predicted_class, "Unknown")
+        # Usar el LabelEncoder para convertir la clase numérica a etiqueta
+        class_label = label_encoder.inverse_transform([predicted_class])[0]
         print(f"Clase predicha (nombre): {class_label}")
 
         return {"prediction": class_label}
--- run_api.sh
+++ run_api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Iniciar Streamlit (Si no funciona ejecutar una primera vez streamlit fuera del script)
-streamlit run front_streamlit.py --server.port=8535 --server.headless=True &
+streamlit run front_streamlit.py --server.port=8534 --server.headless=True &
 
 # Iniciar API (FastAPI)
 uvicorn app:app --host 0.0.0.0 --port 8031 &
