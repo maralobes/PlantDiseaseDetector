2a771fc3828ad9680185d242ab8048f79d50ab1e
Maria <maria.villaloboschaves@gmail.com>
2024-10-23 13:57:31 -0600

    Update frontend image uploading and sppiner

--- front_streamlit.py
+++ front_streamlit.py
@@ -3,16 +3,21 @@ import io
 import requests
 import streamlit as st
 from PIL import Image
+import time
 
 # Definición de la URL de la API
 API_URL = "http://localhost:8031/plantdisease/"
 
 # Streamlit interface, # Configuración de la página
 st.title('Análisis de enfermedades en plantas')
-st.markdown('Sube una imagen de tu planta enferma: \n')
+st.markdown('Sube una imagen de tu planta enferma para que sea analizada:')
 
 # Cargar la imagen
-image_load = st.file_uploader("Sube tu imagen aquí:", type=["jpg", "jpeg", "png"])  # Permitir solo imágenes
+image_load = st.file_uploader("Sube tu imagen aquí (Formatos: JPG, JPEG, PNG):", type=["jpg", "jpeg", "png"])
+
+if image_load is not None:
+    # Mostrar la imagen cargada
+    st.image(image_load, caption="Imagen subida", use_column_width=True)
 
 if st.button('Analizar imagen'):
     if image_load:
@@ -26,12 +31,14 @@ if st.button('Analizar imagen'):
         files = {'file': ('image.png', img_byte_arr, 'image/png')}
         
         # Hacer la solicitud POST a la API
-        response = requests.post(API_URL, files=files)
+        with st.spinner("Analizando la imagen..."):
+            time.sleep(2)  # Espera de 2 segundos para simular procesamiento
+            response = requests.post(API_URL, files=files)
         
         if response.status_code == 200:
             # Procesar los resultados devueltos por la API
             results = response.json()
-            st.write("Predicción de enfermedad:", results.get("prediction"))
+            st.success(f"Predicción de enfermedad: {results.get('prediction')}")
         else:
             st.error("Error en la respuesta de la API")
     else:
